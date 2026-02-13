3e339041789c5626bfc83b08a11093c3a7ac7772
Giovane1607 <101263899+Giovane1607@users.noreply.github.com>
2026-02-13 11:03:09 -0300

    ajustes

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://foundprice.com.br'
+  apiUrl: 'https://api.foundprice.com.br/api'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://foundprice.com.br'
+  apiUrl: 'https://api.foundprice.com.br/api'
 };
